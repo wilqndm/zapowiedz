23bee1c0eddcc4d2419bd43423ff2b558ef391ba
wilqndm <wilq.ndm@gmail.com>
2025-09-09 22:12:11 +0200

    Update teams.ts

--- lib/teams.ts
+++ lib/teams.ts
@@ -7,12 +7,12 @@ export type Team = {
 
     export const teams: Team[] = [
       { id: "nadbuzanka-slopsk", name: "Nadbużanka Słopsk", address: "" },
-      { id: "impet-lajski", name: "Impet Łajski", address: "" },
-      { id: "lider-zakroczym", name: "Lider Zakroczym", address: "" },
+      { id: "impet-lajski", name: "Impet Łajski", address: "Łajski, ul. Sikorskiego 6" },
+      { id: "lider-zakroczym", name: "Lider Zakroczym", address: "Zakroczym, ul. O. H. Koźmińskiego 63" },
       { id: "mewa-krubin", name: "Mewa Krubin", address: "Krubin, ul. Jeziorna 40" },
       { id: "aon-rembertow", name: "AON Rembertów", address: "" },
-      { id: "wisla-jablonna", name: "Wisła Jabłonna", address: "" },
-      { id: "rys-ii-laski", name: "Ryś II Laski", address: "" },
+      { id: "wisla-jablonna", name: "Wisła Jabłonna", address: "Jabłonna, ul. Modlińska 102" },
+      { id: "rys-ii-laski", name: "Ryś II Laski", address: "Laski, ul. Jana Wieczorka 50" },
       { id: "rzadza-zalubice", name: "Rządza Załubice", address: "" },
       { id: "rotavia-nieporet", name: "Rotavia Nieporęt", address: "" },
       { id: "lotnisko-modlin", name: "Lotnisko Modlin", address: "" },
